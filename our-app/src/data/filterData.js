bddaf72ea8d01a83b5316c114d46d39a43c93b0f
jieun419 <crg1050@naver.com>
2023-05-18 10:54:42 +0900

    docs : filterData 불필요한 내용 삭제

--- our-app/src/data/filterData.js
+++ our-app/src/data/filterData.js
@@ -7,9 +7,9 @@ import imgProduct from "../img/filter-product.svg"
 
 
 export const tabContArr = [
-  { name: "전체", type: "all", src: imgAll, content: "1" },
-  { name: "상품", type: "Brand", src: imgBrand, content: "2" },
-  { name: "카테고리", type: "Category", src: imgCategory, content: "3" },
-  { name: "기획전", type: "Exhibition", src: imgExhibition, content: "4" },
-  { name: "브랜드", type: "Product", src: imgProduct, content: "5" },
+  { name: "전체", type: "all", src: imgAll},
+  { name: "상품", type: "Brand", src: imgBrand},
+  { name: "카테고리", type: "Category", src: imgCategory},
+  { name: "기획전", type: "Exhibition", src: imgExhibition},
+  { name: "브랜드", type: "Product", src: imgProduct},
 ];
